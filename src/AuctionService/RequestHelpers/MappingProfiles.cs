ab96d1b4a0809b52959e63e7a80b3adf3842b177
Menake <menakeobhasha1@gmail.com>
2024-04-01 16:42:55 +0530

    End of section 4

--- src/AuctionService/RequestHelpers/MappingProfiles.cs
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -1,6 +1,7 @@
 ﻿using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using Contracts;
 
 namespace AuctionService.RequestHelpers;
 
@@ -12,5 +13,8 @@ public class MappingProfiles : Profile
         CreateMap<Item, AuctionDTO>();
         CreateMap<CreateAuctionDTO, Auction>().ForMember(d => d.Item, o => o.MapFrom(s => s));
         CreateMap<CreateAuctionDTO, Item>();
+        CreateMap<AuctionDTO, AuctionCreated>();
+        CreateMap<Auction, AuctionUpdated>().IncludeMembers(a => a.Item);
+        CreateMap<Item, AuctionUpdated>();
     }
 }
